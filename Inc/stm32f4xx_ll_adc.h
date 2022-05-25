245b4a34d6e8b93fa30fa132601f2b02f128de7e
Ali Labbene <ali.labbene@st.com>
2022-05-25 20:43:22 +0100

    [LL][ADC] Update LL_ADC_DMA_GetRegAddr() API to prevent compilation warning due to unused 'Register' parameter

--- Inc/stm32f4xx_ll_adc.h
+++ Inc/stm32f4xx_ll_adc.h
@@ -1879,6 +1879,9 @@ __STATIC_INLINE uint32_t LL_ADC_DMA_GetRegAddr(ADC_TypeDef *ADCx, uint32_t Regis
 #else
 __STATIC_INLINE uint32_t LL_ADC_DMA_GetRegAddr(ADC_TypeDef *ADCx, uint32_t Register)
 {
+  /* Prevent unused argument compilation warning */
+  (void)Register;
+
   /* Retrieve address of register DR */
   return (uint32_t)&(ADCx->DR);
 }
